908e237e0aaa35df3f0cf3c9f051a54afa255d4e
Doug Steen <douglaspsteen@gmail.com>
2023-03-29 21:26:44 -0600

    changes to my_classes

--- src/my_classes.py
+++ src/my_classes.py
@@ -17,6 +17,6 @@ class MyAge:
 
 
 # instantiate the class and execute the print function
-age = MyAge("1982-08-04", "Mr James")
+age = MyAge("1992-04-28", "Doug")
 print(age.show_me_my_age())
 # > 'Mr James, you are so young, only 39 years old!'
